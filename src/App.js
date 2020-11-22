2656af293e45a99d2250596dce43defdd7c282f3
Ivan Chenoweth <ivanchenoweth@gmail.com>
2020-11-22 15:45:04 -0700

    org-structure update app.js readme

--- src/App.js
+++ src/App.js
@@ -79,8 +79,100 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>CRUD App with Hooks+Axios+storybook V1.2.5</h1>
-      <h2>CICD with netifly</h2>
+      <h1>CRUD React App - GMSO V1.3.0</h1>
+      <h3>Obteniendo una estructura modular</h3>
+
+      <hr></hr>
+
+      <p>
+        Bienvenido a esta versión GMSO V1.3.0 intentamos poder aprender y capacitarnos
+        para elaborar una propuesta para seguir las mejores prácticas.
+      </p>
+      <p>        
+        Es bien sabido que el uso de buenas practicas son para lograr los más altos atributos de calidad en una aplicación en el ciclo de desarrollo de software.
+        Las mejores prácticas que defino aquí no son fórmulas mágicas que solucionarán y mejorarán todos los proyectos. 
+        Pero considero que son una base de ideas y convenciones que creo que vale la pena probar. 
+        Con suerte, pueden ayudarlo a usted y a su equipo a tener una mejor experiencia de desarrollador y, en última instancia, ofrecer un mejor software a la comunidad.
+      </p>
+
+      <hr></hr>
+
+      <p>
+        Entre estas mejores practicas que estamos alcanzando estan en procurar:
+      </p>
+      <ul>
+        <li>Seguir el TDD para evitar escribir codigo innecesario, desarrollo agil entre otros beneficios</li>
+        <li>Crear componentes de forma independiente y mostrar componentes de forma interactiva en un entorno de desarrollo aislado</li>
+        <li>Usar las mínimas librerias, para evitar los conflictos y migraciones</li>
+        <li>Estructurar en forma modular para tener todo escalable (GMSO) </li>
+      </ul>
+
+      <hr></hr>
+
+      <p>
+        Este desarrollo de la estructura modular, esta muy afin a la siguiente lista de guías de enlaces :
+      </p>
+      <ul>
+        <li>
+          <p>
+            <a href="https://medium.com/@alexmngn/how-to-better-organize-your-react-applications-2fd3ea1920f1">
+              Cómo organizar mejor su aplicación de React
+            </a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="https://medium.com/@alexmngn/why-react-developers-should-modularize-their-applications-d26d381854c1">
+              Por qué los desarrolladores deberían modularizar sus aplicaciones
+              en el Frontend
+            </a>
+          </p>
+        </li>
+
+        <li>
+          <p>
+            <a href="https://dev.to/martinrojas/storybook-best-practices-5kk">
+              Mejores practicas para usar componentes en un entorno aislado (TDD)
+            </a>
+          </p>
+        </li>
+
+        
+
+      </ul>
+      <hr></hr>
+
+      <p>Principales librerias usadas (package.json):</p>
+      <ul>
+        <li>Axios</li>
+        <li>Stoyboook</li>
+        <li>Jest</li>
+      </ul>
+
+      <p>Procesos alcanzados en Devop:</p>
+      <ul>
+        <li>CICD with netifly</li>
+        <li>Backend CICD Heroku</li>
+        <li>Stoyboook dashboard deployed</li>
+      </ul>
+
+      <hr></hr>
+
+      <p>Features todo:</p>
+      <ul>
+        <li>Authentications: JWT Auth, Oauth2 + PKCE</li>
+        <li>Router+TDD</li>
+        <li>i18n</li>
+        <li>Yup and Forms</li>
+        <li>Redux/Mobx</li>
+        <li>SonarCube Integration</li>
+        <li>Realtime database and noSQL (RethinkDB/ParseDB/Redis)</li>
+      </ul>
+
+      <hr></hr>
+
+      <p>Variables de ambiente:</p>
+
       <small>
         You are running this application in <b>{process.env.NODE_ENV}</b> mode.
       </small>
@@ -92,9 +184,15 @@ const App = () => {
         Environment REACT_APP_STAGE .env or netlify.toml:{" "}
         {process.env.REACT_APP_STAGE}
       </h6>
-      <Button 
-        label="Button Primary" onClick={() => {alert('test click')}} 
-        primary 
+
+      <hr></hr>
+
+      <Button
+        label="Button Primary"
+        onClick={() => {
+          alert("test click");
+        }}
+        primary
       />
       <div className="flex-row">
         <div className="flex-large">
